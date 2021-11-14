8f53bf12e77ebfee85522db99083456abf14d556
lokesh.d <72072531+131-lk@users.noreply.github.com>
2021-11-14 20:01:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,11 @@
-#!/usr/bin/env python
 from flask import Flask, request
 
-# create app
 app = Flask(_name_)
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'GET':
-        # show html form
         return '''
             <form method="post">
                 <input type="text" name="expression" />
@@ -16,11 +13,9 @@ def index():
             </form>
         '''
     elif request.method == 'POST':
-        # calculate result
         expression = request.form.get('expression')
         result = eval(expression)
         return 'result: %s' % result
 
-# run app
 if _name_ == '_main_':
     app.run(debug=True)
